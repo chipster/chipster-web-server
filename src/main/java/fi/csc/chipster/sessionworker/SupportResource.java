59a2aef573091e489e75d7ac5bf5c3e8587cd3ac
klemela <klemela>
2018-12-19 15:59:16 +0200

    Fix the responsse content type of the support request

--- src/main/java/fi/csc/chipster/sessionworker/SupportResource.java
+++ src/main/java/fi/csc/chipster/sessionworker/SupportResource.java
@@ -13,7 +13,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -67,7 +66,6 @@ public class SupportResource {
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	@RolesAllowed(Role.CLIENT)
 	@Path("request")
 	@Transaction
