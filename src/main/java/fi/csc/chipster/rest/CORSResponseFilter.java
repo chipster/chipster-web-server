0ab52c70b986eb85054b62c886d326fa36905462
klemela <klemela>
2019-04-26 13:50:12 +0300

    Remove the body of the preflight responses

--- src/main/java/fi/csc/chipster/rest/CORSResponseFilter.java
+++ src/main/java/fi/csc/chipster/rest/CORSResponseFilter.java
@@ -7,8 +7,11 @@ import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
+
+import org.glassfish.grizzly.http.Method;
 
 @Provider
 @Priority(Priorities.HEADER_DECORATOR)
@@ -26,6 +29,11 @@ public class CORSResponseFilter implements ContainerResponseFilter {
 		headers.add("Access-Control-Expose-Headers", "location, Accept-Ranges, Retry-After"); // response
 		headers.add("Access-Control-Allow-Credentials", "true");
 		headers.add("Access-Control-Max-Age", "1728000"); // in seconds, 20 days
-		//headers.add("Access-Control-Max-Age", "1"); // makes debugging easier			
+		//headers.add("Access-Control-Max-Age", "1"); // makes debugging easier
+				
+		if (Method.OPTIONS.matchesMethod(requestContext.getMethod())) {
+			// otherwise Jersey responds with some xml that Chrome doesn't like: chrome cross-origin read blocking blocked cross-origin response with mime type application/vnd.sun.wadl+xml"
+			responseContext.setEntity(null, null, MediaType.APPLICATION_JSON_TYPE);
+		}
 	}
 }
