c03eae56e9c587454a151de87d7a1b38d96d1caf
Taavi Hupponen <taavi.hupponen@csc.fi>
2019-08-07 14:02:27 +0300

    Do not use String.blank() from java11

--- src/main/java/fi/csc/chipster/auth/resource/JwsUtils.java
+++ src/main/java/fi/csc/chipster/auth/resource/JwsUtils.java
@@ -21,46 +21,50 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 public class JwsUtils {
-	
-private static Logger logger = LogManager.getLogger();
-	
+
+	private static Logger logger = LogManager.getLogger();
+
 	private static final String KEY_JWS_PRIVATE_KEY = "jws-private-key";
 	private static final String KEY_JWS_ALGORITHM = "jws-algorithm";
-	
+
 	public static SignatureAlgorithm getSignatureAlgorithm(Config config, String role) {
 		return SignatureAlgorithm.forName(config.getString(KEY_JWS_ALGORITHM, role));
 	}
-	
-	public static KeyPair getOrGenerateKeyPair(Config config, String role, SignatureAlgorithm signatureAlgorithm) throws PEMException {
+
+	public static KeyPair getOrGenerateKeyPair(Config config, String role, SignatureAlgorithm signatureAlgorithm)
+			throws PEMException {
 		String privateKeyString = config.getString(KEY_JWS_PRIVATE_KEY, role);
-		
+
 		KeyPair keyPair = null;
-		if (!privateKeyString.isBlank()) {
-			
+		if (!privateKeyString.trim().isEmpty()) {
+
 			logger.info("use configured jws private key");
 			// the library generates the public key automatically from the private key
-			keyPair = JwsUtils.pemToKeyPair(privateKeyString);			
-	
+			keyPair = JwsUtils.pemToKeyPair(privateKeyString);
+
 		} else {
-		
+
 			logger.warn("jws private key not configured, generate new");
 			/*
-			 *  Generating a new key is secure, but may cause authentication errors, 
-			 *  when the auth is restarted.
-			 *  
-			 *  Command for generating a fixed key (https://connect2id.com/products/nimbus-jose-jwt/openssl-key-generation):
-			 *  openssl ecparam -genkey -name secp521r1 -noout -out ec512-key-pair.pem
-			 *  
-			 */			
+			 * Generating a new key is secure, but may cause authentication errors, when the
+			 * auth is restarted.
+			 * 
+			 * Command for generating a fixed key
+			 * (https://connect2id.com/products/nimbus-jose-jwt/openssl-key-generation):
+			 * openssl ecparam -genkey -name secp521r1 -noout -out ec512-key-pair.pem
+			 * 
+			 */
 			keyPair = Keys.keyPairFor(signatureAlgorithm);
 		}
-		
-		logger.info(role + "jws private key:" + keyPair.getPrivate().getAlgorithm() + " " + keyPair.getPrivate().getFormat());
-		logger.info(role + "jws public key:" + keyPair.getPublic().getAlgorithm() + " " + keyPair.getPublic().getFormat());
-		
+
+		logger.info(role + "jws private key:" + keyPair.getPrivate().getAlgorithm() + " "
+				+ keyPair.getPrivate().getFormat());
+		logger.info(
+				role + "jws public key:" + keyPair.getPublic().getAlgorithm() + " " + keyPair.getPublic().getFormat());
+
 		return keyPair;
 	}
-	
+
 	/**
 	 * Convert byte array key to PEM format
 	 * 
@@ -68,18 +72,18 @@ private static Logger logger = LogManager.getLogger();
 	 * @return
 	 */
 	public static String publicKeyToPem(PublicKey publicKey) {
-		
+
 		StringWriter writer = new StringWriter();
-		
+
 		try (PemWriter pemWriter = new PemWriter(writer)) {
-	        pemWriter.writeObject(new PemObject("PUBLIC KEY", publicKey.getEncoded()));
-	    } catch (IOException e) {
+			pemWriter.writeObject(new PemObject("PUBLIC KEY", publicKey.getEncoded()));
+		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		return writer.toString();
 	}
-	
+
 	public static Object parsePem(String pem) {
 		PEMParser pemParser = new PEMParser(new StringReader(pem));
 		try {
@@ -91,19 +95,19 @@ private static Logger logger = LogManager.getLogger();
 				pemParser.close();
 			} catch (IOException e) {
 				throw new RuntimeException(e);
-			}			
+			}
 		}
 	}
-	
-	public static KeyPair pemToKeyPair(String pem) throws PEMException{
-				
-		PEMKeyPair pemKeyPair = (PEMKeyPair) parsePem(pem);			
+
+	public static KeyPair pemToKeyPair(String pem) throws PEMException {
+
+		PEMKeyPair pemKeyPair = (PEMKeyPair) parsePem(pem);
 		JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
 		return converter.getKeyPair(pemKeyPair);
 	}
-	
-	public static PublicKey pemToPublicKey(String pem) throws PEMException{
-		
+
+	public static PublicKey pemToPublicKey(String pem) throws PEMException {
+
 		SubjectPublicKeyInfo publicKeyInfo = (SubjectPublicKeyInfo) parsePem(pem);
 		JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
 		return converter.getPublicKey(publicKeyInfo);
