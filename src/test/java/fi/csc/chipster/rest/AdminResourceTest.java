3780e6348513a5b82a39623dad9ba214e9161186
Taavi Hupponen <taavi.hupponen@csc.fi>
2019-11-06 12:28:58 +0200

    Remove reference springframework assert

--- src/test/java/fi/csc/chipster/rest/AdminResourceTest.java
+++ src/test/java/fi/csc/chipster/rest/AdminResourceTest.java
@@ -1,6 +1,7 @@
 package fi.csc.chipster.rest;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,7 +16,6 @@ import javax.ws.rs.core.Response;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.util.Assert;
 
 import fi.csc.chipster.auth.model.Role;
 
@@ -26,97 +26,93 @@ public class AdminResourceTest {
 
 	private static Config config;
 
-    @BeforeClass
-    public static void setUp() throws Exception {
-    	config = new Config();
-    	launcher = new TestServerLauncher(config);        
-    }
+	@BeforeClass
+	public static void setUp() throws Exception {
+		config = new Config();
+		launcher = new TestServerLauncher(config);
+	}
 
-    @AfterClass
-    public static void tearDown() throws Exception {
-    	launcher.stop();
-    }
-    
-    public Set<String> getRolesWithAdminUrl() {
-    	return config.getAdminServiceUrls().keySet();
-    }
-    
-    @Test
-    public void getStatus() throws IOException {
-    	for (String role : getRolesWithAdminUrl()) {
-    		if (Role.BACKUP.equals(role)) {
-    			// the backup service doesn't offer status information
-    			continue;
-    		}
-    		getStatus(role);
-    	}
-    }
-    
-    @Test
-    public void getAlive() throws IOException {
-    	for (String role : getRolesWithAdminUrl()) {
-    		System.out.println(role);
-    		// doesn't require authentication
-    		assertEquals(200, getAdminResponse(launcher.getNoAuthClient(), role, "alive").getStatus());
-    	}
-    }
-    
-    @Test
-    public void noAuth() throws IOException {
-    	for (String role : getRolesWithAdminUrl()) {
-    		if (Role.TYPE_SERVICE.equals(role)) {
-    			//FXIME type service doesn't authenticate 
-    			continue;
-    		}
-    		
-     		assertEquals(403, getStatusResponse(launcher.getUser1Client(), role).getStatus());
-     		assertEquals(403, getStatusResponse(launcher.getNoAuthClient(), role).getStatus());
-     		assertEquals(403, getStatusResponse(launcher.getWrongTokenClient(), role).getStatus());
-     		assertEquals(401, getStatusResponse(launcher.getUnparseableTokenClient(), role).getStatus());
-    	}
-    }
-    
-    @Test 
-    public void sessionDbTopics() throws IOException {
-    	assertEquals(403, getAdminResponse(launcher.getUser1Client(), Role.SESSION_DB, "topics").getStatus());
-    	assertEquals(403, getAdminResponse(launcher.getNoAuthClient(), Role.SESSION_DB, "topics").getStatus());
- 		assertEquals(403, getAdminResponse(launcher.getWrongTokenClient(), Role.SESSION_DB, "topics").getStatus());
- 		assertEquals(401, getAdminResponse(launcher.getUnparseableTokenClient(), Role.SESSION_DB, "topics").getStatus());
-    }
-    
-    public void getStatus(String role) throws IOException {
-    	Assert.notNull(getStatusMap(launcher.getMonitoringClient(), role).size());
-    }
-    
+	@AfterClass
+	public static void tearDown() throws Exception {
+		launcher.stop();
+	}
+
+	public Set<String> getRolesWithAdminUrl() {
+		return config.getAdminServiceUrls().keySet();
+	}
+
+	@Test
+	public void getStatus() throws IOException {
+		for (String role : getRolesWithAdminUrl()) {
+			if (Role.BACKUP.equals(role)) {
+				// the backup service doesn't offer status information
+				continue;
+			}
+			getStatus(role);
+		}
+	}
+
+	@Test
+	public void getAlive() throws IOException {
+		for (String role : getRolesWithAdminUrl()) {
+			System.out.println(role);
+			// doesn't require authentication
+			assertEquals(200, getAdminResponse(launcher.getNoAuthClient(), role, "alive").getStatus());
+		}
+	}
+
+	@Test
+	public void noAuth() throws IOException {
+		for (String role : getRolesWithAdminUrl()) {
+			if (Role.TYPE_SERVICE.equals(role)) {
+				// FXIME type service doesn't authenticate
+				continue;
+			}
+
+			assertEquals(403, getStatusResponse(launcher.getUser1Client(), role).getStatus());
+			assertEquals(403, getStatusResponse(launcher.getNoAuthClient(), role).getStatus());
+			assertEquals(403, getStatusResponse(launcher.getWrongTokenClient(), role).getStatus());
+			assertEquals(401, getStatusResponse(launcher.getUnparseableTokenClient(), role).getStatus());
+		}
+	}
+
+	@Test
+	public void sessionDbTopics() throws IOException {
+		assertEquals(403, getAdminResponse(launcher.getUser1Client(), Role.SESSION_DB, "topics").getStatus());
+		assertEquals(403, getAdminResponse(launcher.getNoAuthClient(), Role.SESSION_DB, "topics").getStatus());
+		assertEquals(403, getAdminResponse(launcher.getWrongTokenClient(), Role.SESSION_DB, "topics").getStatus());
+		assertEquals(401,
+				getAdminResponse(launcher.getUnparseableTokenClient(), Role.SESSION_DB, "topics").getStatus());
+	}
+
+	public void getStatus(String role) throws IOException {
+		assertNotNull(getStatusMap(launcher.getMonitoringClient(), role).size());
+	}
+
 	public HashMap<String, Object> getStatusMap(Client client, String role) throws IOException {
-    	return getMap(getStatusResponse(client, role));
-	}
-    
-	@SuppressWarnings("unchecked")
-    private HashMap<String, Object> getMap(Response response) throws IOException {
-		return RestUtils.parseJson(HashMap.class, RestUtils.toString((InputStream)response.getEntity()));
+		return getMap(getStatusResponse(client, role));
 	}
 
-	public Response getStatusResponse(Client client, String role) throws IOException {    	
-    	Response response = getAdminResponse(client, role, "status");
-    	
-    	return response;
+	@SuppressWarnings("unchecked")
+	private HashMap<String, Object> getMap(Response response) throws IOException {
+		return RestUtils.parseJson(HashMap.class, RestUtils.toString((InputStream) response.getEntity()));
 	}
-    
-    public Response getAdminResponse(Client client, String role, String path) throws IOException {    	
-    	Response response = getJson(
-    			client.target(config.getAdminServiceUrls().get(role))
-    			.path("admin")
-    			.path(path));
-    	
-    	return response;
+
+	public Response getStatusResponse(Client client, String role) throws IOException {
+		Response response = getAdminResponse(client, role, "status");
+
+		return response;
 	}
-    
-    public Response getJson(WebTarget target) {
-    	Response response = target
-    			.request(JSON)
-    		    .get(Response.class);
-    	
-    	return response;
-    }
+
+	public Response getAdminResponse(Client client, String role, String path) throws IOException {
+		Response response = getJson(client.target(config.getAdminServiceUrls().get(role)).path("admin").path(path));
+
+		return response;
+	}
+
+	public Response getJson(WebTarget target) {
+		Response response = target.request(JSON).get(Response.class);
+
+		return response;
+	}
 }
