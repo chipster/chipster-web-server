5c028825d34e7a3ac6e17e7f31da44f962731ae6
Petri Klemela <petri.klemela@csc.fi>
2021-08-05 14:42:20 +0300

    Swap finished scripts

--- src/main/resources/bash-job-scheduler/k3s/finished.bash
+++ src/main/resources/bash-job-scheduler/k3s/finished.bash
@@ -1,1 +1,2 @@
-echo "nothing to do"
+job_id=$(echo comp-job-$SESSION_ID-$JOB_ID | cut -c 1-63)
+kubectl --kubeconfig conf/kubeconfig delete job $job_id
--- src/main/resources/bash-job-scheduler/process/finished.bash
+++ src/main/resources/bash-job-scheduler/process/finished.bash
@@ -1,2 +1,1 @@
-job_id=$(echo comp-job-$SESSION_ID-$JOB_ID | cut -c 1-63)
-kubectl --kubeconfig conf/kubeconfig delete job $job_id
+echo "nothing to do"
