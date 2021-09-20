f92eb6794256d7a0af22a468e9039edb59fe7bf0
Petri Klemela <petri.klemela@csc.fi>
2021-09-20 12:45:27 +0300

    Simplify jq syntax

--- src/main/resources/bash-job-scheduler/k3s/run.bash
+++ src/main/resources/bash-job-scheduler/k3s/run.bash
@@ -9,19 +9,14 @@ memory=$(( SLOTS*8 ))Gi
 cpu_limit=$(( SLOTS*2 ))
 cpu_request=$SLOTS
 
-cpu="2"
-memory="100M"
-job_json=$(echo "$stdin" | yq r - --tojson | jq '
-    .spec.metadata.name='"$job_id"' |
-    .spec.containers[0].image='"$IMAGE"' |
-    .spec.containers[0].command[+]='"$SESSION_ID"' |
-    .spec.containers[0].command[+]='"$JOB_ID"' |
-    .spec.containers[0].command[+]='"$SESSION_TOKEN"' |
-    .spec.containers[0].command[+]='"$COMP_TOKEN"' |
-    .spec.containers[0].resources.limits.cpu='"$cpu_limit"' | 
-    .spec.containers[0].resources.limits.memory="'"$memory"'" |
-    .spec.containers[0].resources.requests.cpu='"$cpu_request"' | 
-    .spec.containers[0].resources.requests.memory="'"$memory"'"')
+jq_patch=".metadata.name=\"$job_id\" |
+  .spec.containers[0].image=\"$IMAGE\" |
+  .spec.containers[0].command += [\"$SESSION_ID\", \"$JOB_ID\", \"$SESSION_TOKEN\", \"$COMP_TOKEN\"] |
+  .spec.containers[0].resources.limits.cpu=\"$cpu_limit\" | 
+  .spec.containers[0].resources.limits.memory=\"$memory\" |
+  .spec.containers[0].resources.requests.cpu=\"$cpu_request\" | 
+  .spec.containers[0].resources.requests.memory=\"$memory\""
 
-echo "$job_json"
+job_json=$(echo "$stdin" | yq e - -o=json | jq "$jq_patch")
+
 echo "$job_json" | kubectl apply -f -
