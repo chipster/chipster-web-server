d4556294c05d36ae4f8b1e99e749a2baf9ccfd70
Petri Klemela <petri.klemela@csc.fi>
2024-03-14 15:10:18 +0200

    Explain design decisions

--- src/main/java/fi/csc/chipster/s3storage/ChipsterEncryption.java
+++ src/main/java/fi/csc/chipster/s3storage/ChipsterEncryption.java
@@ -18,6 +18,117 @@ import javax.crypto.spec.IvParameterSpec;
 
 import org.apache.commons.io.IOUtils;
 
+/**
+ * <h1>Encrypt and decrypt file streams</h1>
+ * 
+ * <h2>Thread model</h2>
+ * <p>
+ * Encryption at rest is a great idea, especially when saving data in object
+ * storage, where a simple mistake in bucket configuration can make all data
+ * publicly readable.
+ * </p>
+ * 
+ * <h2>Encryption scheme</h2>
+ * 
+ * <p>
+ * A standard way for this would be the PGP file format, and
+ * there is even a nice Java implementation for it. Unfortunately the
+ * performance of this Java implementation was unsatisfactory, < 100 MiB/s
+ * decryption on x64 architectures (see Appendix 1). The native gpg client
+ * performs better (around 200 MiB/s), but it would be cumbersome to pass
+ * Java streams through it, especially considering its stateful key management
+ * interface.
+ * </p>
+ * 
+ * <p>
+ * More modern inspiration could be taken from TLS and it's use of AES-256-GCM,
+ * but it shouldn't be used for records larger than 64 GB
+ * (https://crypto.stackexchange.com/questions/31793/plain-text-size-limits-for-aes-gcm-mode-just-64gb).
+ * TLS apparently gets around this by diving the message into 16kiB records.
+ * However, there doesn't seem to exists a standard file format for storing a
+ * stream of GCM records on disk. Additionally, it would be daunting to rotate
+ * the encryption IV ourselves.
+ * </p>
+ * 
+ * <p>
+ * The CBC mode seems to offer a good balance here. It doesn't have known
+ * catastrophic vulnerabilities, when we create a separate random key
+ * and IV for each file. It doesn't offer authentication, but that's not a
+ * problem if our primary thread model is the accidental publication of the
+ * object storage bucket. It neither has a standard file format, but without
+ * practical file size limit
+ * (https://crypto.stackexchange.com/questions/51518/maximum-number-of-blocks-to-be-encrypted-under-one-key-in-cbc-and-ctr-mode),
+ * we can simply concatenate the IV and the ciphertext, without the need for
+ * any additional record structures.
+ * </p>
+ * 
+ * <h2>Key management</h2>
+ * 
+ * <p>
+ * We can easily generate a new random key for each file and store it in the
+ * database. This makes the key available for anyone, who would have acces to
+ * the file data anyway and satisfies our thread model above. It has several
+ * additional advantages:
+ * <ul>
+ * <li>We don't have any internal keys that we would have to manage
+ * <li>Having an own key for each file makes the encryption more resistant
+ * against attacks.
+ * <li>We avoid all the difficulties of asymmetric cryptography and password
+ * based key derivation functions
+ * <li>This makes it trivial to offload the encryption and decryption to
+ * anywhere where the access to the data is needed anyway (client app, comp,
+ * session-worker).
+ * </ul>
+ * </p>
+ * 
+ * <h2>Appendix 1</h2>
+ * 
+ * <p>
+ * Performance of the pgp file format in Java ("java encrypt pgp", "java decrypt
+ * pgp") was tested on several processor architectures. The test was based on
+ * example code in
+ * https://github.com/bcgit/bc-java/blob/main/pg/src/main/java/org/bouncycastle/
+ * , but the following changes were made to improve the troughput:
+ * <ul>
+ * <li>>armor: false,
+ * <li>integrity: true
+ * <li>CEST5 -> AES256,
+ * <li>without BC provider
+ * </ul>
+ * </p>
+ * 
+ * <p>
+ * "java encrypt aes" and "java encrypt aes" refer to the current
+ * implementation. "native gpg encrypt" was measured with
+ * GnuPG version 2.2.19 on x64 and version 2.4.3 on Apple M1:
+ * <code>cat tmp/rand | gpg --output - --compress-algo none --recipient
+ * RECIPIENT_NAME --always-trust --cipher-algo AES256 --encrypt - | pv >
+ * tmp/rand_gpg_enc</code>
+ * </p>
+ * 
+ * Apple M1:
+ * 
+ * java encrypt pgp 294 MB/s
+ * java decrypt pgp 146 MB/s
+ * java encrypt aes 588 MB/s
+ * java decrypt aes 1154 MB/s
+ * native gpg encrypt: 204 MB/s
+ * 
+ * Intel Skylake:
+ * 
+ * java encrypt pgp 73 MB/s
+ * java decrypt pgp 33 MB/s
+ * java encrypt aes 148 MB/s
+ * java decrypt aes 230 MB/s
+ * native gpg encrypt: 165 MB/s
+ * 
+ * AMD EPYC:
+ * 
+ * java encrypt pgp 188 MB/s
+ * java decrypt pgp 82 MB/s
+ * java encrypt aes 408 MB/s
+ * java decrypt aes 662 MB/s
+ */
 public class ChipsterEncryption {
 
     // other programs can't read our files anyway, so let's make it easier for us to
@@ -27,52 +138,6 @@ public class ChipsterEncryption {
     // AES/CBC/PKCS5Padding
     public static final String CHIPSTER_ENC_SIG = "chipster-encrypted-file-v1.";
 
-    /*
-     * Why to use AES directly and not PGP?
-     * ------------------------------------
-     * 
-     * PGP would be nice, because it provides a standardized file format. However,
-     * its speed in Java on x64 is a bit slow.
-     * 
-     * how java pgp was tested: armor: false, integrity: true, CEST5 -> AES256,
-     * without BC provider
-     * https://github.com/bcgit/bc-java/blob/main/pg/src/main/java/org/bouncycastle/
-     * openpgp/examples/KeyBasedLargeFileProcessor.java
-     * 
-     * Apple M1:
-     * 
-     * java encrypt pgp 294 MB/s
-     * java decrypt pgp 146 MB/s
-     * java encrypt aes 588 MB/s
-     * java decrypt aes 1154 MB/s
-     * native gpg encrypt: 204 MB/s
-     * 
-     * Intel Skylake:
-     * 
-     * java encrypt pgp 73 MB/s
-     * java decrypt pgp 33 MB/s
-     * java encrypt aes 148 MB/s
-     * java decrypt aes 230 MB/s
-     * native gpg encrypt: 165 MB/s
-     * 
-     * AMD EPYC:
-     * 
-     * java encrypt pgp 188 MB/s
-     * java decrypt pgp 82 MB/s
-     * java encrypt aes 408 MB/s
-     * java decrypt aes 662 MB/s
-     * 
-     * Why CBC?
-     * --------
-     * 
-     * - no practical max data size like in GCM
-     * https://crypto.stackexchange.com/questions/51518/maximum-number-of-blocks-to-
-     * be-encrypted-under-one-key-in-cbc-and-ctr-mode
-     * - it's enough to keep the data secret if s3 access control leaks, we don't
-     * really care about message authentication
-     * - creating a separate random key and iv for each file should reduce remaining
-     * risks
-     */
     public static final String V1_ALGORITHM = "AES/CBC/PKCS5Padding";
     public static final int V1_IV_SIZE = 16;
 
