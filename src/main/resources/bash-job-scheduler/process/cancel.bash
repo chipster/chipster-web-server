ce1f90d83f33536b18bec9920068f97335e7001c
Petri Klemela <petri.klemela@csc.fi>
2025-03-11 15:25:58 +0200

    Fix cancel

--- src/main/resources/bash-job-scheduler/process/cancel.bash
+++ src/main/resources/bash-job-scheduler/process/cancel.bash
@@ -1,9 +1,9 @@
-pid=$(ps aux | grep SingleShotComp | grep -v grep | grep $POD_NAME | tr -s ' ' | cut -d ' ' -f 2)
+pid=$(ps aux | grep SingleShotComp | grep -v grep | grep -v "bash -c" | grep $JOB_ID | tr -s ' ' | cut -d ' ' -f 2)
 found=$(echo "$pid" | wc -l)
 if [ -z "$pid" ]; then
     echo 'process not found'
 elif [ $found == 1 ]; then
-    echo "kill $pid"
+    echo "kill: $pid"
     kill $pid
 else
     echo "found $found processes"
