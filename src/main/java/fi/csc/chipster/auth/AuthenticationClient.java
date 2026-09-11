0ee648da0dea266d0b72b439f2b9503e7dea3362
Taavi Hupponen <taavi.hupponen@csc.fi>
2026-09-11 08:21:38 +0000

    Use the internal address of auth also for the user lookups
    AuthenticationClient.getUser() and getUsers() took the address from the public
    service list, although everything else in the class uses getAuth(), which
    prefers the internal address when the client belongs to a server. The methods
    were added as test helpers and session-worker started to use one of them nine
    months later, so nobody ever chose the public address for a server.
    
    It worked anyway, because both addresses reach the same auth server, but not
    when the public address is relative: then Jersey fails with "URI is not
    absolute".
    
    The static methods take a WebTarget now, like RestMethods and the rest of this
    class, so they don't have to know about addresses at all. That's also all the
    tests need, because the launcher builds such targets already.

--- src/main/java/fi/csc/chipster/auth/AuthenticationClient.java
+++ src/main/java/fi/csc/chipster/auth/AuthenticationClient.java
@@ -333,14 +333,25 @@ public class AuthenticationClient {
 		};
 	}
 
+	/**
+	 * Get a user
+	 * 
+	 * Like the other methods of this class, this uses the internal address of auth
+	 * when the instance was created for a server, see getAuth(). Then the
+	 * ServiceLocatorClient must have its credentials set.
+	 */
 	public User getUser(UserId userId) throws RestException {
-		return AuthenticationClient.getUser(userId, getAuthenticatedClient(), serviceLocator);
+		return getUser(userId, getAuthenticatedClient().target(getAuth()));
 	}
 
-	public static User getUser(UserId userId, Client client, ServiceLocatorClient serviceLocator) throws RestException {
+	/**
+	 * Get a user from the given address of auth
+	 * 
+	 * For callers without an AuthenticationClient instance, like the tests.
+	 */
+	public static User getUser(UserId userId, WebTarget authTarget) throws RestException {
 		try {
-			return RestMethods.get(client
-					.target(serviceLocator.getPublicUri(Role.AUTH))
+			return RestMethods.get(authTarget
 					.path(AuthUserResource.USERS)
 					.queryParam(AuthUserResource.USER_ID_KEY,
 							URLEncoder.encode(userId.toUserIdString(), StandardCharsets.UTF_8.name())),
@@ -352,13 +363,15 @@ public class AuthenticationClient {
 		}
 	}
 
+	/**
+	 * Get all users, see getUser()
+	 */
 	public List<User> getUsers() throws RestException {
-		return AuthenticationClient.getUsers(getAuthenticatedClient(), serviceLocator);
+		return getUsers(getAuthenticatedClient().target(getAuth()));
 	}
 
-	public static List<User> getUsers(Client client, ServiceLocatorClient serviceLocator) throws RestException {
-		return RestMethods.getList(client
-				.target(serviceLocator.getPublicUri(Role.AUTH))
+	public static List<User> getUsers(WebTarget authTarget) throws RestException {
+		return RestMethods.getList(authTarget
 				.path(AuthUserResource.USERS), User.class);
 	}
 
